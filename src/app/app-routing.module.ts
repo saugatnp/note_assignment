268f27f6b6fa6b092eef07ec7f223a525ea1369e
Saugat Adhikari <saugat@mediprocomputers.com>
2022-07-19 00:42:03 +0545

    data added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotePageComponent } from './content/note-page/note-page.component';
+import { TagsComponent } from './content/tags/tags.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: NotePageComponent},
+ { path: 'Home', component: NotePageComponent},
+ { path: 'Tags', component: TagsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotePageComponent } from './content/note-page/note-page.component';
+import { NavigationComponent } from './content/navigation/navigation.component';
+import { RouterModule } from '@angular/router';
+import { TagsComponent } from './content/tags/tags.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotePageComponent,
+    NavigationComponent,
+    TagsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
